1bc9fe290652513402fd314f9a3db961f99c6330
Martin Chalupa <mchalupa@netflix.com>
2019-10-29 12:40:52 -0700

    Update build script

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
 # This script will build the project.
 
-SWITCHES="--info --stacktrace"
-
-GRADLE_VERSION=$(./gradlew -version | grep Gradle | cut -d ' ' -f 2)
-
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$GRADLE_PUBLISH" == "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew build $SWITCHES
+  ./gradlew build
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew build $SWITCHES
+  ./gradlew -Prelease.travisci=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" build snapshot
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   case "$TRAVIS_TAG" in
   *-rc\.*)
-    ./gradlew -Prelease.travisci=true -Psonatype.username=${sonatypeUsername} -Psonatype.password=${sonatypePassword} -Pbintray.user=${bintrayUser} -Pbintray.apiKey=${bintrayKey}  -Prelease.useLastTag=true candidate $SWITCHES
+    ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" candidate
     ;;
   *)
-    ./gradlew -Prelease.travisci=true -Psonatype.username=${sonatypeUsername} -Psonatype.password=${sonatypePassword} -Pbintray.user=${bintrayUser} -Pbintray.apiKey=${bintrayKey}  -Prelease.useLastTag=true final $SWITCHES
+    ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" final
     ;;
   esac
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ./gradlew build $SWITCHES
+  ./gradlew build
 fi
-
-EXIT=$?
-
-rm -f "$HOME/.gradle/caches"
-rm -rf "$HOME/.gradle/cachesn"
-
-exit $EXIT

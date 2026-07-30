5946b2e6daf930ac35be85a1928e6318cf71b572
John Burns <johnburns@netflix.com>
2026-07-30 16:09:59 -0500

    don't consider nodep -> project a diff
    since project dependencies are not really "locked", when a new one shows up, it should not be new nor updated, it should be considered not a diff

---
+++ src/main/java/com/netflix/nebula/dependencies/comparison/StringUtil.java
@@ -0,0 +1,14 @@
+package com.netflix.nebula.dependencies.comparison;
+
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
+@NullMarked
+public class StringUtil {
+    /**
+     * versions can be null if they are project dependencies, which should be considered as "not locked"
+     */
+    public static boolean isNullOrEmpty(@Nullable String versionString) {
+        return versionString == null || versionString.isEmpty();
+    }
+}
